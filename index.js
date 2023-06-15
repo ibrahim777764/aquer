21df50fbf01a43312416c409c03bea03681c00f9
unknown <ibrahimnaseer896@gmail.com>
2023-06-15 14:45:54 -0700

    updated version

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const slides = document.querySelectorAll('.slide');
 const buttons = document.querySelectorAll('.switch-button');
+const slider = document.querySelector('.slider');
+const slideWidth = slides[0].clientWidth;
+
+let currentSlide = 0;
+let intervalId;
 
 buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
     buttons.forEach(btn => btn.classList.remove('active'));
     button.classList.add('active');
-    const slideWidth = slides[0].clientWidth;
-    document.querySelector('.slider').style.transform = `translateX(-${slideWidth * index}px)`;
+    slider.style.transform = `translateX(-${slideWidth * index}px)`;
+    currentSlide = index;
+    clearInterval(intervalId); // Stop the automatic sliding
   });
 });
+
+function goToNextSlide() {
+  currentSlide++;
+  if (currentSlide === slides.length) {
+    currentSlide = 0;
+  }
+  slider.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+  buttons.forEach(btn => btn.classList.remove('active'));
+  buttons[currentSlide].classList.add('active');
+}
+
+function startAutoAnimation() {
+  intervalId = setInterval(goToNextSlide, 5000); // Interval duration in milliseconds (change as needed)
+}
+
+startAutoAnimation();
